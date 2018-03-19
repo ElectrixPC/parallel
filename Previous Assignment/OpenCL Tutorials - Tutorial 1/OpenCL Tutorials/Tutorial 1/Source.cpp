0946429d629b6301df65564eda753992e7c377de
James Allington-Kay <lancaster.1@hotmail.co.uk>
2018-03-19 18:41:06 +0000

    As previous commit

--- Previous Assignment/OpenCL Tutorials - Tutorial 1/OpenCL Tutorials/Tutorial 1/Source.cpp
+++ Previous Assignment/OpenCL Tutorials - Tutorial 1/OpenCL Tutorials/Tutorial 1/Source.cpp
@@ -83,27 +83,29 @@ int main(int argc, char **argv) {
 		input_file.close();
 
 		
-		vector<double> metrics(3);
-		vector<int> lines(size);
+		
+		//vector<int> lines(size);
 
-		// define size of buffers for OpenCL
-		cl::Buffer buffer_valCheck(context, CL_MEM_READ_WRITE, sizeBytes);
-		cl::Buffer buffer_outCheck(context, CL_MEM_READ_WRITE, sizeIDXBytes);
-		//Copy array to device memory
-		cl::Event valCheck_event;
-		queue.enqueueWriteBuffer(buffer_valCheck, CL_TRUE, 0, sizeBytes, &inputElements[0], NULL, &valCheck_event);
-		// Create kernel instance
-		cl::Kernel kernel_groupdata = cl::Kernel(program, "groupdata");
-		// Set arguments for kernel (in and out)
-		kernel_groupdata.setArg(0, buffer_valCheck);
-		kernel_groupdata.setArg(1, buffer_outCheck);
-		// Run kernel
-		cl::Event prof_groupdata;
-		queue.enqueueNDRangeKernel(kernel_groupdata, cl::NullRange, cl::NDRange(size), cl::NullRange, NULL, &prof_groupdata);
-		// Retrieve output from OpenCL
-		queue.enqueueReadBuffer(buffer_outCheck, CL_TRUE, 0, size, &lines[0]);
+		//// define size of buffers for OpenCL
+		//cl::Buffer buffer_valCheck(context, CL_MEM_READ_WRITE, sizeBytes);
+		//cl::Buffer buffer_outCheck(context, CL_MEM_READ_WRITE, sizeIDXBytes);
+		////Copy array to device memory
+		//cl::Event valCheck_event;
+		//queue.enqueueWriteBuffer(buffer_valCheck, CL_TRUE, 0, sizeBytes, &inputElements[0], NULL, &valCheck_event);
+		//// Create kernel instance
+		//cl::Kernel kernel_groupdata = cl::Kernel(program, "groupdata");
+		//// Set arguments for kernel (in and out)
+		//kernel_groupdata.setArg(0, buffer_valCheck);
+		//kernel_groupdata.setArg(1, buffer_outCheck);
+		//// Run kernel
+		//cl::Event prof_groupdata;
+		//queue.enqueueNDRangeKernel(kernel_groupdata, cl::NullRange, cl::NDRange(size), cl::NullRange, NULL, &prof_groupdata);
+		//// Retrieve output from OpenCL
+		//queue.enqueueReadBuffer(buffer_outCheck, CL_TRUE, 0, size, &lines[0]);
 
 		// do this serial as the cost to transfer the data is higher
+
+
 		int currmin = 99999;
 		int prev = 0;
 		for (int i = 0; i < size; i++) {
@@ -112,40 +114,68 @@ int main(int argc, char **argv) {
 				if (currmin > prev) {
 					currmin = prev;
 				}
-				prev = 0;
+				prev = 0;;
 			}
-			
 		}
-		
+		int lines = size / currmin;
+		vector<int> metrics(pow(25, 2));
 		float stepsize = currmin - 2;
-		vector<long long int> dates(lines[0]);
-		std::size_t sizeLINTBytes = size * sizeof(long long int);
-		std::size_t sizeFLTBytes = (lines[0] * 2) * sizeof(float);
-		vector<float> temps(lines[0] * 2);
+		//vector<long long int> dates(lines);
+		//std::size_t sizeLINTBytes = size * sizeof(long long int);
+		std::size_t sizeFLTBytes = (lines * 1.1) * sizeof(float);
+		vector<float> temps(lines);
 		// define size of buffers for OpenCL
 		cl::Buffer buffer_val(context, CL_MEM_READ_WRITE, sizeBytes);
 		cl::Buffer buffer_out(context, CL_MEM_READ_WRITE, sizeFLTBytes);
-		cl::Buffer buffer_dates(context, CL_MEM_READ_WRITE, sizeLINTBytes);
-		cl::Buffer buffer_metrics(context, CL_MEM_READ_WRITE, 3*sizeof(double));
+		//cl::Buffer buffer_dates(context, CL_MEM_READ_WRITE, sizeLINTBytes);
+		cl::Buffer buffer_metrics(context, CL_MEM_READ_WRITE, pow(25,2)*sizeof(int));
 		//Copy array to device memory
 		cl::Event val_event;
 		cl::Event idx_event;
 		queue.enqueueWriteBuffer(buffer_val, CL_TRUE, 0, sizeBytes, &inputElements[0], NULL, &val_event);
 		// Create kernel instance
-		cl::Kernel kernel_splitdata = cl::Kernel(program, "splitdata");
+		cl::Kernel kernel_splitdata = cl::Kernel(program, "splithistdata");
 		// Set arguments for kernel (in and out)
 		kernel_splitdata.setArg(0, stepsize);
 		kernel_splitdata.setArg(1, buffer_val);
 		kernel_splitdata.setArg(2, buffer_out);
-		kernel_splitdata.setArg(3, buffer_dates);
-		kernel_splitdata.setArg(4, buffer_metrics);
+		//kernel_splitdata.setArg(3, buffer_dates);
+		kernel_splitdata.setArg(3, buffer_metrics);
 		// Run kernel
 		cl::Event prof_splitdata;
 		queue.enqueueNDRangeKernel(kernel_splitdata, cl::NullRange, cl::NDRange(size), cl::NullRange, NULL, &prof_splitdata);
 		// Retrieve output from OpenCL
-		queue.enqueueReadBuffer(buffer_out, CL_TRUE, 0, (lines[0] * 2), &temps[0]);
-		queue.enqueueReadBuffer(buffer_dates, CL_TRUE, 0, lines[0], &dates[0]);
-		queue.enqueueReadBuffer(buffer_metrics, CL_TRUE, 0, 3, &metrics[0]);
+		queue.enqueueReadBuffer(buffer_out, CL_TRUE, 0, lines, &temps[0]);
+		//queue.enqueueReadBuffer(buffer_dates, CL_TRUE, 0, lines[0], &dates[0]);
+		queue.enqueueReadBuffer(buffer_metrics, CL_TRUE, 0, pow(25,2), &metrics[0]);
+
+
+		std::cout << "Time taken to run program [ms]:" << timer.End() << endl;
+
+
+		vector<float> mymax(lines);
+		vector<float> mymin(lines);
+
+		cl::Event max_event;
+		cl::Event min_event;
+
+		cl::Buffer buffer_max(context, CL_MEM_READ_WRITE, sizeFLTBytes);
+		cl::Buffer buffer_min(context, CL_MEM_READ_WRITE, sizeFLTBytes);
+		cl::Kernel kernel_mymax = cl::Kernel(program, "mymax");
+		cl::Kernel kernel_mymin = cl::Kernel(program, "mymin");
+		kernel_mymax.setArg(0, buffer_out);
+		kernel_mymax.setArg(1, buffer_max);
+		cl::Event prof_mymax;
+		queue.enqueueNDRangeKernel(kernel_mymax, cl::NullRange, cl::NDRange(lines), cl::NullRange, NULL, &prof_mymax);
+		queue.enqueueReadBuffer(buffer_max, CL_TRUE, 0, lines, &mymax[0]);
+
+		kernel_mymin.setArg(0, buffer_out);
+		kernel_mymin.setArg(1, buffer_min);
+
+		cl::Event prof_mymin;
+		queue.enqueueNDRangeKernel(kernel_mymin, cl::NullRange, cl::NDRange(lines), cl::NullRange, NULL, &prof_mymin);
+		queue.enqueueReadBuffer(buffer_min, CL_TRUE, 0, lines, &mymin[0]);
+
 		long timetaken;
 		timetaken = (prof_splitdata.getProfilingInfo<CL_PROFILING_COMMAND_END>() - prof_splitdata.getProfilingInfo<CL_PROFILING_COMMAND_START>()) / 1000000; // Kernel execution time
 		timetaken += (val_event.getProfilingInfo<CL_PROFILING_COMMAND_END>() - val_event.getProfilingInfo<CL_PROFILING_COMMAND_START>()) / 1000000; // Memory transfer time
@@ -154,137 +184,6 @@ int main(int argc, char **argv) {
 		std::cout << "Time taken to run program [ms]:" << timer.End() << endl;
 		std::getchar();
 
-		//std::string root(".");
-		//string file_path = "temp_lincolnshire.txt";
-		//typedef int T;
-		//std::vector<T> data = Parse::File<T>(file_path);
-		//std::cout << "Size: " << data.size() << ", Last: " << data.back() << '\n' << std::endl;
-
-		//std::ifstream inputFile("temp_lincolnshire.txt", std::ios::in | std::ios::binary | std::ios::ate);
-		////Size of File (By calling the current character which is the end)
-		//std::size_t elements = inputFile.tellg();
-		//size_t size = elements * sizeof(int);//size in bytes
-		////Seek back to the start of the file
-		//inputFile.seekg(0, std::ios_base::beg);
-		////Define char array and then read file into array
-		//char * inputElements = new char[elements];
-		//int index = 0;
-		//while(!inputFile.eof()) {
-		//	inputFile.ignore(elements, '\n');
-
-		//	char word[7] = { "     \0" };
-		//	int j = inputFile.gcount(), counter = 6;
-
-		//	//Go back to front for efficieny
-		//	while (inputElements[j] != ' ')
-		//		*(word + counter--) = inputElements[j--];
-		//	//inputFile.readsome(inputElements, index+3);
-		//	index++;
-		//}
-
-		//inputFile.read(&inputElements[0], size);
-		////Close file to save memory
-		//inputFile.close();
-		//// Initialise output array
-		//
-
-		///*for (int i = 0; i < inputF.len; i++) {
-
-		//Part 4 - memory allocation
-		//host - input
-
-		//ifstream inputFile("temp_lincolnshire.txt");
-		//string line;
-		//
-
-		//int size = count_line(inputFile);
-		//int counter = 0;
-		//std::vector<float> temps(size);
-
-		//if (inputFile.is_open())
-		//{
-		//	while (getline(inputFile, line))
-		//	{
-
-		//		temps[counter] = std::stof(find_last(line));
-		//		cout << temps[counter] << '\n';
-		//	}
-		//	inputFile.close();
-		//}*/
-
-		//
-
-		//vector<int> A = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; //C++11 allows this type of initialisation
-		//vector<int> B = { 0, 1, 2, 0, 1, 2, 0, 1, 2, 0 };
-		//
-		//std::vector<int>A(10000000);
-		//std::vector<int>B(10000000);
-
-		//size_t vector_elements = A.size();//number of elements
-		//size_t vector_size = A.size()*sizeof(int);//size in bytes
-
-		////host - output
-		//vector<int> C(vector_elements);
-
-		////device - buffers
-		//cl::Buffer buffer_A(context, CL_MEM_READ_WRITE, vector_size);
-		//cl::Buffer buffer_B(context, CL_MEM_READ_WRITE, vector_size);
-		//cl::Buffer buffer_C(context, CL_MEM_READ_WRITE, vector_size);
-
-		////Part 5 - device operations
-
-		////5.1 Copy arrays A and B to device memory
-		//cl::Event A_event;
-		//cl::Event B_event;
-		//queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, vector_size, &A[0], NULL, &A_event);
-		//queue.enqueueWriteBuffer(buffer_B, CL_TRUE, 0, vector_size, &B[0], NULL, &B_event);
-
-		////5.2 Setup and execute the kernel (i.e. device code)
-		////cl::Kernel kernel_add = cl::Kernel(program, "add");
-		////cl::Kernel kernel_mult = cl::Kernel(program, "multi");
-		//cl::Kernel kernel_multiadd = cl::Kernel(program, "multiadd");
-
-		////kernel_mult.setArg(0, buffer_A);
-		////kernel_mult.setArg(1, buffer_B);
-		////kernel_mult.setArg(2, buffer_C);
-		////kernel_add.setArg(0, buffer_C);
-		////kernel_add.setArg(1, buffer_B);
-		////kernel_add.setArg(2, buffer_C);
-
-		//kernel_multiadd.setArg(0, buffer_A);
-		//kernel_multiadd.setArg(1, buffer_B);
-		//kernel_multiadd.setArg(2, buffer_C);
-
-
-
-		////cl::Event prof_multevent;
-		////queue.enqueueNDRangeKernel(kernel_mult, cl::NullRange, cl::NDRange(vector_elements), cl::NullRange, NULL, &prof_multevent);
-		////cl::Event prof_event;
-		////queue.enqueueNDRangeKernel(kernel_add, cl::NullRange, cl::NDRange(vector_elements), cl::NullRange, NULL, &prof_event);
-
-		//cl::Event prof_event;
-		//queue.enqueueNDRangeKernel(kernel_multiadd, cl::NullRange, cl::NDRange(vector_elements), cl::NullRange, NULL, &prof_event);
-
-
-		////5.3 Copy the result from device to host
-		//queue.enqueueReadBuffer(buffer_C, CL_TRUE, 0, vector_size, &C[0]);
-
-		//cout << "A = " << A << endl;
-		//cout << "B = " << B << endl;
-		//cout << "C = " << C << endl;
-
-		////std::cout << "Kernel execution time [ns]:" << prof_multevent.getProfilingInfo<CL_PROFILING_COMMAND_END>() - prof_multevent.getProfilingInfo<CL_PROFILING_COMMAND_START>() << std::endl;
-		////std::cout << GetFullProfilingInfo(prof_multevent, ProfilingResolution::PROF_US) << endl;
-
-
-		//std::cout << "Kernel execution time [ns]:" << prof_event.getProfilingInfo<CL_PROFILING_COMMAND_END>() - prof_event.getProfilingInfo<CL_PROFILING_COMMAND_START>() << std::endl;
-		//std::cout << GetFullProfilingInfo(prof_event, ProfilingResolution::PROF_US) << endl;
-
-		//std::cout << "Transfer time A buffer [ns]:" << A_event.getProfilingInfo<CL_PROFILING_COMMAND_END>() - A_event.getProfilingInfo<CL_PROFILING_COMMAND_START>() << std::endl;
-		//std::cout << GetFullProfilingInfo(A_event, ProfilingResolution::PROF_US) << endl;
-
-		//std::cout << "Transfer time B buffer [ns]:" << B_event.getProfilingInfo<CL_PROFILING_COMMAND_END>() - B_event.getProfilingInfo<CL_PROFILING_COMMAND_START>() << std::endl;
-		//std::cout << GetFullProfilingInfo(B_event, ProfilingResolution::PROF_US) << endl;
 	}
 	catch (cl::Error err) {
 		cerr << "ERROR: " << err.what() << ", " << getErrorString(err.err()) << endl;
