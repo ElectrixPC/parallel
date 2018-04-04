d39ebfb1741aaef26750505eb7cd9cf06d0bf4c5
James Allington-Kay <lancaster.1@hotmail.co.uk>
2018-04-04 19:03:27 +0100

    Fixing mean values and adding output
    Needs a polish and adding some more queries on the kernel side for choosing location

--- Previous Assignment/OpenCL Tutorials - Tutorial 1/OpenCL Tutorials/Tutorial 1/Source.cpp
+++ Previous Assignment/OpenCL Tutorials - Tutorial 1/OpenCL Tutorials/Tutorial 1/Source.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 #include <fstream>
 #include "Timer.hpp"
 #include <CL/cl.hpp>
@@ -18,6 +19,23 @@ void print_help() {
 	cerr << "  -d : select device" << endl;
 	cerr << "  -l : list all platforms and devices" << endl;
 	cerr << "  -h : print this message" << endl;
+}
+
+string getHistOutput(int size, int total) {
+
+	string output = "";
+	float percent = (float)size / (float)total;
+	int actualpercent = percent * 100;
+	int sizelength = 10 - to_string(size).length();
+
+	for (int i = 0; i < sizelength; i++) {
+		output += " ";
+	}
+	for (int i = 0; i < actualpercent; i++) {
+		output += "=";
+	}
+	
+	return output;
 }
 
 void parseGetHistMetrics(char* inputElements, int size, cl::Context context, cl::CommandQueue queue, cl::Program program) {
@@ -141,13 +159,170 @@ vector<float> justParseData(char* inputElements, int size, cl::Context context, 
 	return temps;
 }
 
+
+vector<float> histParseData(char* inputElements, int size, cl::Context context, cl::CommandQueue queue, cl::Program program) {
+
+	vector<unsigned int> minline(1);
+	int sizeBytes = size * sizeof(char);
+
+	vector<int> hist(pow(25, 2));
+	vector<float> metrics(pow(25, 2));
+	cl::Buffer buffer_val(context, CL_MEM_READ_ONLY, sizeBytes);
+	cl::Buffer buffer_minline(context, CL_MEM_READ_WRITE, 1 * sizeof(unsigned int));
+	cl::Event val_event;
+
+	queue.enqueueWriteBuffer(buffer_val, CL_TRUE, 0, sizeBytes, &inputElements[0], NULL, &val_event);
+
+	cl::Kernel kernel_getminline = cl::Kernel(program, "getminline");
+	kernel_getminline.setArg(0, buffer_val);
+	kernel_getminline.setArg(1, buffer_minline);
+	cl::Event prof_getminline;
+	queue.enqueueNDRangeKernel(kernel_getminline, cl::NullRange, cl::NDRange(size), cl::NullRange, NULL, &prof_getminline);
+	// Retrieve output from OpenCL
+	queue.enqueueReadBuffer(buffer_minline, CL_TRUE, 0, 1, &minline[0]);
+
+	long timetakenMin;
+	timetakenMin = (prof_getminline.getProfilingInfo<CL_PROFILING_COMMAND_END>() - prof_getminline.getProfilingInfo<CL_PROFILING_COMMAND_START>()) / 1000000; // Kernel execution time
+	timetakenMin += (val_event.getProfilingInfo<CL_PROFILING_COMMAND_END>() - val_event.getProfilingInfo<CL_PROFILING_COMMAND_START>()) / 1000000; // Memory transfer time
+
+	std::cout << "Time taken to get line min [ms]:" << timetakenMin << endl;
+	// TODO CALCULATE THE MIN LINES TO GET ALL THE VALUES
+	int lines = size / 5;
+
+	float stepsize = minline[0] - 2;
+	std::size_t sizeFLTBytes = (lines * 1.1) * sizeof(float);
+	vector<float> temps(lines);
+	// define size of buffers for OpenCL
+
+	cl::Buffer buffer_out(context, CL_MEM_READ_WRITE, sizeFLTBytes);
+	cl::Buffer buffer_hist(context, CL_MEM_READ_WRITE, pow(25, 2) * sizeof(float));
+	cl::Buffer buffer_metrics(context, CL_MEM_READ_WRITE, pow(25, 2) * sizeof(float));
+	//Copy array to device memory
+	cl::Event idx_event;
+	// Create kernel instance
+	cl::Kernel kernel_parsehistdata = cl::Kernel(program, "outhistdata");
+	// Set arguments for kernel (in and out)
+	kernel_parsehistdata.setArg(0, stepsize);
+	kernel_parsehistdata.setArg(1, buffer_val);
+	kernel_parsehistdata.setArg(2, buffer_out);
+	kernel_parsehistdata.setArg(3, buffer_hist);
+	kernel_parsehistdata.setArg(4, buffer_metrics);
+	// Run kernel
+	cl::Event prof_parsehistdata;
+
+	queue.enqueueNDRangeKernel(kernel_parsehistdata, cl::NullRange, cl::NDRange(size), cl::NullRange, NULL, &prof_parsehistdata);
+	// Retrieve output from OpenCL
+	queue.enqueueReadBuffer(buffer_out, CL_TRUE, 0, lines, &temps[0]);
+	queue.enqueueReadBuffer(buffer_hist, CL_TRUE, 0, pow(25, 2), &hist[0]);
+	queue.enqueueReadBuffer(buffer_metrics, CL_TRUE, 0, pow(25, 2), &metrics[0]);
+	
+	float totalSize = (hist[0] + hist[1] + hist[2] + hist[3] + hist[4]);
+	long timetakenParse;
+	timetakenParse = (prof_parsehistdata.getProfilingInfo<CL_PROFILING_COMMAND_END>() - prof_parsehistdata.getProfilingInfo<CL_PROFILING_COMMAND_START>()) / 1000000; // Kernel execution time
+	std::cout << "Time taken to run kernel [ms]:" << timetakenParse << endl;
+	timetakenParse = (val_event.getProfilingInfo<CL_PROFILING_COMMAND_END>() - val_event.getProfilingInfo<CL_PROFILING_COMMAND_START>()) / 1000000; // Memory transfer time
+	std::cout << "Time taken to transfer mem [ms]:" << timetakenParse << endl;
+	std::cout << "---------------- Histogram -------------------" << endl;
+	std::cout << "Bin 1: " << hist[0] << getHistOutput(hist[0], totalSize) << endl;
+	std::cout << "Bin 2: " << hist[1] << getHistOutput(hist[1], totalSize) << endl;
+	std::cout << "Bin 3: " << hist[2] << getHistOutput(hist[2], totalSize) << endl;
+	std::cout << "Bin 4: " << hist[3] << getHistOutput(hist[3], totalSize) << endl;
+	std::cout << "Bin 5: " << hist[4] << getHistOutput(hist[4], totalSize) << endl;
+	std::cout << endl;
+	std::cout << "Min value: " << metrics[0] << endl;
+	std::cout << "Max value: " << metrics[1] << endl;
+
+	temps[lines - 1] = totalSize;
+
+	return temps;
+}
+
+vector<float> getSorted(vector<float> temps, int lines, cl::Context context, cl::CommandQueue queue, cl::Program program)
+{
+
+	////float actualSize = temps[lines - 1];
+	////temps[lines - 1] = 0.0;
+	int local_size = 1024;
+
+	size_t padding_size = temps.size() % local_size;
+	
+	//if the input vector is not a multiple of the local_size
+	//insert additional neutral elements (0 for addition) so that the total will not be affected
+	if (padding_size) {
+		//create an extra vector with neutral values
+		std::vector<int> A_ext(local_size - padding_size, 0);
+		//append that extra vector to our input
+		temps.insert(temps.end(), A_ext.begin(), A_ext.end());
+	}
+
+	lines = temps.size();
+	std::size_t sizeFLTBytes = lines * sizeof(int);
+	cl::Buffer data_buffer(context, CL_MEM_READ_WRITE, sizeFLTBytes);
+	cl::Buffer sort_buffer(context, CL_MEM_READ_WRITE, sizeFLTBytes);
+	cl::Event mean_event;
+
+	//queue.enqueueWriteBuffer(buffer_temps, CL_TRUE, 0, lines, &temps[0], NULL, &mean_event);
+
+	//cl::Kernel kernel_sortdata = cl::Kernel(program, "sort_bitonic");
+	//// Set arguments for kernel (in and out)
+	//kernel_sortdata.setArg(0, buffer_temps);
+
+
+	//cl::Event prof_sortdata;
+
+	//queue.enqueueNDRangeKernel(kernel_sortdata, cl::NullRange, cl::NDRange(lines), cl::NullRange, NULL, &prof_sortdata);
+	//// Retrieve output from OpenCL
+	//queue.enqueueReadBuffer(buffer_temps, CL_TRUE, 0, lines, &temps[0]);
+
+	//// TODO get the number of lines to get the mean value
+
+	//return temps;
+
+		int merge = 0;
+
+	queue.enqueueWriteBuffer(data_buffer, CL_TRUE, 0, lines, &temps[0], NULL, &mean_event);
+	queue.enqueueFillBuffer(sort_buffer, 0, 0, sizeFLTBytes);
+	//Configure kernels and queue them for execution
+	cl::Kernel sort_kernel = cl::Kernel(program, "sort_FLOAT");
+	sort_kernel.setArg(0, data_buffer);
+	sort_kernel.setArg(1, sort_buffer);
+	sort_kernel.setArg(2, cl::Local(local_size * sizeof(float)));
+	sort_kernel.setArg(3, merge);
+
+	//Create vector to read final values
+	vector<float> output(lines);
+	cl::Event prof_sortdata;
+	//Initially call the kernel so that the input buffer can be replaced with the modified/sorted data
+	queue.enqueueNDRangeKernel(sort_kernel, cl::NullRange, cl::NDRange(lines), cl::NDRange(local_size), NULL, &prof_sortdata);
+
+	sort_kernel.setArg(0, sort_buffer);
+
+	do {
+			//Flip the merge flag and set it
+			merge = !merge;
+			sort_kernel.setArg(3, merge);
+
+			//Queue and execute the kernel
+			queue.enqueueNDRangeKernel(sort_kernel, cl::NullRange, cl::NDRange(lines), cl::NDRange(local_size), NULL, &prof_sortdata);
+
+			//Read the buffer and wait for it to finish then check if the kernel is sorted
+			//If not, repeat from top of the loop
+		queue.enqueueReadBuffer(sort_buffer, CL_TRUE, 0, lines, &output[0]);
+		queue.finish();
+	} while (!std::is_sorted(output.begin(), output.end()));
+
+	return output;
+}
+
 float getMeanValue(vector<float> temps, int lines, cl::Context context, cl::CommandQueue queue, cl::Program program)
 {
+	
+	float actualSize = temps[lines - 1];
+	temps[lines - 1] = 0.0;
 	vector<float> mean(lines);
-	std::size_t sizeFLTBytes = lines  * sizeof(float);
+	std::size_t sizeFLTBytes = lines * sizeof(float);
 	cl::Buffer buffer_temps(context, CL_MEM_READ_WRITE, sizeFLTBytes);
 	cl::Buffer buffer_mymean(context, CL_MEM_READ_WRITE, sizeFLTBytes);
-
 	cl::Event mean_event;
 
 	queue.enqueueWriteBuffer(buffer_temps, CL_TRUE, 0, lines, &temps[0], NULL, &mean_event);
@@ -166,9 +341,10 @@ float getMeanValue(vector<float> temps, int lines, cl::Context context, cl::Comm
 	queue.enqueueReadBuffer(buffer_mymean, CL_TRUE, 0, lines, &mean[0]);
 
 	// TODO get the number of lines to get the mean value
-	float meanValue = mean[0] / (lines);
 
-	std::cout << meanValue << endl;
+	float meanValue = mean[0] / (actualSize);
+
+	std::cout << "Mean Value: " << meanValue << endl;
 
 	return meanValue;
 }
@@ -240,9 +416,13 @@ int main(int argc, char **argv) {
 
 		//vector<unsigned int> minline(1);
 		//vector<float> temps = justParseData(inputElements, size, context, queue, program);
-		//float mean = getMeanValue(temps, temps.size(), context, queue, program);
+		vector<float> temps = histParseData(inputElements, size, context, queue, program);
+		//vector<int> temps = {1, 2, 4, 7, 12, 11, 6, 4, 2, 1};
+		//temps = getSorted(temps, temps.size(), context, queue, program); 
+		std::cout << "Time taken to parse data & get metrics file [ms]:" << timer.End() << endl;
+		float mean = getMeanValue(temps, temps.size(), context, queue, program);
 		//parseGetHist(inputElements, size, context, queue, program);
-		parseGetHistMetrics(inputElements, size, context, queue, program);
+		//parseGetHistMetrics(inputElements, size, context, queue, program);
 		std::cout << "Total time taken to complete everything [ms]:" << timer.End() << endl;
 
 		//cl::Buffer buffer_val(context, CL_MEM_READ_ONLY, sizeBytes);
